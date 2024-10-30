ccf5da1c8e2579587bc4167cdd1fd6a6a836f092
coco <comit@xelope.fun>
2024-10-30 19:12:19 +0800

    first upload

---
+++ HelloWorld/HelloWorld/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def hello(request):
+    return HttpResponse('Hello World!')
+
+def test(request):
+    context = {}
+    context = {'hello':'A test charsequence!','name':'Cooc'}
+    return render(request, 'test.html', context)
